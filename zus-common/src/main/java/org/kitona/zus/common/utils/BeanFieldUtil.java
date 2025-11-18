ea86909944095b8652c1f2575a7fd7f3b509823c
kitona <kitona.liu@kitona.org>
2025-11-18 14:57:54 +0800

    fix(zus-commons): 修复bean复制工具在17中无法转换的问题
    Orika工具在jdk17及更高中无法正常使用，使用Mapstruct替换
    
    Closes https://github.com/orika-mapper/orika/issues/395
    
    [skip ci]

--- zus-common/src/main/java/org/kitona/zus/common/utils/BeanFieldUtil.java
+++ zus-common/src/main/java/org/kitona/zus/common/utils/BeanFieldUtil.java
@@ -10,7 +10,6 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
