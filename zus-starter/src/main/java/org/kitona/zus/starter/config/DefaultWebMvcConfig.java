f447892b69419bdcf55336d3c0848ceee9bfea7a
kitona <2092669791@qq.com>
2026-03-18 00:11:13 +0800

    feat(zus-starter): 配置全局CORS跨域支持

--- zus-starter/src/main/java/org/kitona/zus/starter/config/DefaultWebMvcConfig.java
+++ zus-starter/src/main/java/org/kitona/zus/starter/config/DefaultWebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.kitona.zus.starter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -10,5 +11,15 @@ public class DefaultWebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MdcHandlerInterceptor());
+    }
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
